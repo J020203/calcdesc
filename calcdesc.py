dceb37ed5ffe1f5a4d2b28fb2acba6ea5df30c7d
J <juanchagu66@gmail.com>
2021-01-21 19:22:14 -0500

    Reprogramming

--- calcdesc.py
+++ calcdesc.py
@@ -10,28 +10,28 @@ parser.add_argument("-p", "--precio", help = "Indica el precio del producto.")
 parser.add_argument("-d", "--descuento", help = "Indica el descuento que se le aplica al precio.")
 parser.add_argument("-v", "--version", action = 'version' ,version='%(prog)s 1.0')
 args = parser.parse_args()
-    
+
+def discount(precio, desc):
+    ans = float(precio - (precio * (desc / 100)))
+    return ans
+
 try:
     if args.precio:
-        x = int(args.precio)
+        precio = int(args.precio)
     else:
-        x = int(input("Ingrese el precio del producto: "))
+        precio = int(input("Ingrese el precio del producto: "))
+
     if args.descuento:
-        y = int(args.descuento)
+        desc = int(args.descuento)
     else:
-        y = int(input("Ingrese el descuento: "))    
+        desc = int(input("Ingrese el descuento: "))    
 
-    if (x >= 0 and y >=0):        
-        def discount(x, y):
-            if (x % 2 == 0):
-                ans = int(abs(x-(x*(y/100))))
-            else:
-                ans = float(abs(x-(x*(y/100))))
-            print(f'El precio real de ${x} con {y}% de descuento es ${ans}')    
-
-        discount(x, y)
+    if (precio >= 0 and desc >=0):
+        ans = discount(precio, desc)
+        print(f'El precio real de ${precio} con {desc}% de descuento es ${ans}')    
     else:
         print("El precio y el descuento deben de ser positivos.")
+
 except KeyboardInterrupt:
     print("\nHas detenido el programa.")
 except ValueError:
